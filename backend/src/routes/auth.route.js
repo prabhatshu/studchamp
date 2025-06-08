c38a39e861673a884a64e4b02043f99c3b233b7a
prabhatshu <shuklaprabhatprabhat@gmail.com>
2025-06-09 02:19:12 +0530

    initial commmit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { logout,login, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { onboard } from "../controllers/auth.controller.js";
+const router =express.Router();
+
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.post("/onboarding",protectRoute, onboard);
+
+router.get("/me", protectRoute, (req, res)=>{
+    res.status(200).json({success:true, user:req.user});
+})
+
+export default router
+
