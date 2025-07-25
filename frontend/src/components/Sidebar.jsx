0f56d80091af64cf1aa3946089a1deec62b25431
Prabhat Shukla <135755925+prabhatshu@users.noreply.github.com>
2025-07-25 11:33:53 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -12,9 +12,7 @@ const Sidebar = () => {
       <div className="p-5 border-b border-base-300">
         <Link to="/" className="flex items-center gap-2.5">
           
-          <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-           StudChamp
-          </span>
+        
         </Link>
       </div>
 
